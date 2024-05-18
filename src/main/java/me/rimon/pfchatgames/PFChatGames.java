7b4c8149218559b2f421056ceeb6c80fdc631ebc
BuildTools <unconfigured@null.spigotmc.org>
2024-05-18 07:53:27 -0500

    Initial commit

---
+++ src/main/java/me/rimon/pfchatgames/PFChatGames.java
@@ -0,0 +1,17 @@
+package me.rimon.pfchatgames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PFChatGames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
